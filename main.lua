0edb24ef77702cc8818da8be77018a5c5a324dcb
Mayur Patil <mayurpatil001@hotmail.com>
2019-11-05 22:36:19 +0530

    The Collision Update

--- main.lua
+++ main.lua
@@ -30,6 +30,8 @@ local spawnTimer = 0
 
 local lastY = -PIPE_HEIGHT + math.random(80) + 20
 
+local scrolling = true
+
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
@@ -48,31 +50,39 @@ end
 
 
 function love.update(dt)
-    backgroundScorll = (backgroundScorll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
-    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % GROUND_LOOPING_POINT
 
-    spawnTimer = spawnTimer + dt
+    if scrolling then
+        backgroundScorll = (backgroundScorll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT
+        groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % GROUND_LOOPING_POINT
 
-    if spawnTimer > 2 then
-        local y = math.max(-PIPE_HEIGHT + 10,
-            math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
-        lastY = y
-        table.insert(pipePairs, PipePair(y))
-        spawnTimer = 0
-    end
+        spawnTimer = spawnTimer + dt
 
-    bird:update(dt)
+        if spawnTimer > 2 then
+            local y = math.max(-PIPE_HEIGHT + 10,
+                math.min(lastY + math.random(-20, 20), VIRTUAL_HEIGHT - 90 - PIPE_HEIGHT))
+            lastY = y
+            table.insert(pipePairs, PipePair(y))
+            spawnTimer = 0
+        end
 
-    for k, pipePair in pairs(pipePairs) do
-        pipePair:update(dt)
-    end
-    
-    for k, pipePair in pairs(pipePairs) do
-        if pipePair.remove then
-            table.remove(pipePairs, k)
+        bird:update(dt)
+
+        for k, pipePair in pairs(pipePairs) do
+            pipePair:update(dt)
+            
+            for l, pipe in pairs(pipePair.pipes) do
+                if bird:collides(pipe) then
+                    scrolling = false
+                end
+            end
+        end
+        
+        for k, pipePair in pairs(pipePairs) do
+            if pipePair.remove then
+                table.remove(pipePairs, k)
+            end
         end
     end
-
     love.keyboard.keysPressed = {}
 end
 
